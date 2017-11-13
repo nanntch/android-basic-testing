1d1653d4bec7a1043417afc8c57a255f0219eb45
Natcha <58070069@kmitl.ac.th>
2017-11-14 02:42:57 +0700

    name is null

--- app/src/main/java/cc/somkiat/basicunittesting/NameValidation.java
+++ app/src/main/java/cc/somkiat/basicunittesting/NameValidation.java
@@ -2,13 +2,27 @@ package cc.somkiat.basicunittesting;
 
 class NameValidation {
 
-    public String validate(String name){
-        return name;
-    }
+    public String validate(String name) {
+        String validateText = "Name validation is success";
+
+        if (isNull(name) == false) {
+            validateText = "Name is Null";
+        } else if (isEmpty(name) == false) {
+            validateText = "Name is empty";
+        }
+
+        return validateText;
+}
 
     public boolean isEmpty(String name) {
+        if (name.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 
-        if(name.isEmpty()){
+    public boolean isNull(String name) {
+        if (name == null) {
             return false;
         }
         return true;
--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
@@ -2,6 +2,7 @@ package cc.somkiat.basicunittesting;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 
 public class NameValidationFailTest {
@@ -9,7 +10,18 @@ public class NameValidationFailTest {
     @Test
     public void ชื่อเป็นค่าว่าง(){
         NameValidation validation = new NameValidation();
+        //โยน input เข้าไป check
         boolean result = validation.isEmpty("");
         assertFalse("ต้องไม่ผ่านนะ เพราะว่ามันมีค่าว่าง!", result);
+        assertEquals("Name is empty", validation.validate(""));
     }
+
+    @Test
+    public void ชื่อเป็นค่าnull(){
+        NameValidation validation = new NameValidation();
+        boolean result = validation.isNull(null);
+        assertFalse("ต้องไม่ผ่านนะ เพราะว่ามันเป็นค่า null!", result);
+        assertEquals("Name is Null", validation.validate(null));
+    }
+    
 }
